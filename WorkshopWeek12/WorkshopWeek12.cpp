5b76b6c76020354fd68b7313d45ffedc4ab6cbe6
scottmac-uni <a1745541@adelaide.edu.au>
2026-05-25 14:47:32 +0930

    init

---
+++ WorkshopWeek12/WorkshopWeek12.cpp
@@ -0,0 +1,12 @@
+﻿// WorkshopWeek12.cpp : Defines the entry point for the application.
+//
+
+#include "WorkshopWeek12.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
